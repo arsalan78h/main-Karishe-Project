ecf0a6ddcbc662d9467735fe615b4ac4c986febe
arsalan78h <arsalan78h@gmail.com>
2019-09-17 17:36:27 +0430

    Complete Categories Lists

--- UI-KARISHE/AppDelegate.swift
+++ UI-KARISHE/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+      //  print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         //MARK: - LogOut from account
       //  KeychainWrapper.standard.removeAllKeys()
